2105d374e46c3342e6d96bc82cdf82e5569c05db
edwinmongare <edwinmongare15@gmail.com>
2021-09-09 22:30:09 +0300

    added api

--- testDeployment.js
+++ testDeployment.js
@@ -1,38 +1,64 @@
-// Include Nodejs' net module.
 const Net = require("net");
-// The port on which the server is listening.
+const axios = require("axios");
+const { stringify } = require("querystring");
 const port = 5000;
 
-// Use net.createServer() in your code. This is just for illustration purpose.
-// Create a new TCP server.
 const server = new Net.Server();
-// The server listens to a socket for a client to make a connection request.
-// Think of a socket as an end point.
 server.listen(port, function () {
   console.log(
     `Server listening for connection requests on socket localhost:${port}`
   );
 });
 
-// When a client requests a connection with the server, the server creates a new
-// socket dedicated to that client.
 server.on("connection", function (socket) {
   console.log("A new connection has been established.");
-
-  // Now that a TCP connection has been established, the server can send data to
-  // the client by writing to its socket.
-  socket.write("Hello, client.");
-
   // The server can also receive data from the client by reading from its socket.
   socket.on("data", function (chunk) {
-    console.log(`Data received from client: ${chunk.toString()}`);
+    telemtryDataArray = [];
+    const rawdata = JSON.parse(chunk);
+    const telemetryData = JSON.parse(chunk);
+    console.log(telemetryData);
+    telemtryDataArray.push(telemetryData);
+    for (let telemtry of telemtryDataArray) {
+      telemtry = {
+        imei: telemtry["imei_no"],
+        deviceRuntime: telemtry["device_runtime"],
+        batteryLevel: telemtry["battery_level"],
+        liters: telemtry["litres_total"],
+        aquiferLevel: telemtry["aquifer_level"],
+        knocksCount: telemtry["knocks_count"],
+        staticWaterLevel: telemtry["static_water_level"],
+        totalDepth: telemtry["total_depth"],
+      };
+      console.log(`Data received: ${JSON.stringify(telemtry)}`);
+
+      //**  post to http endpoint
+      axios
+        .post(
+          "https://bahari2dev.azurewebsites.net/api/Borehole/SaveSensorTelemetry",
+          JSON.stringify(telemtry),
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        )
+        .then(
+          (response) => {
+            console.log("tcp message", response.data);
+            console.log("responseData Axios", response.status);
+          },
+          (error) => {
+            console.log("errorData Axios", error.message);
+          }
+        );
+    }
   });
 
   socket.on("end", function () {
     console.log("Closing connection with the client");
   });
 
-  // Don't forget to catch error, for your own sake.
   socket.on("error", function (err) {
     console.log(`Error: ${err}`);
   });
